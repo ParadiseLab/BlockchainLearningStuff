3a155f71d643ccdaac27983b66daf295d04c04ec
Brian Sinquin <ppxlab@gmail.com>
2021-07-26 22:56:22 +0200

    added base data structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ParadiseLab/BlockchainLearningStuff
+
+go 1.16
--- go_blockchain_POW/main.go
+++ go_blockchain_POW/main.go
@@ -1,15 +1,61 @@
+package main
+
+// imports
+import (
+	"crypto/sha256"
+	"time"
+)
+
 // Create transaction struct
+type Transaction struct {
+	address_from [32]byte
+	address_to   [32]byte
+	data         string
+	amount       float32
+}
+
 // Create block struct
+type Block struct {
+	nonce        uint64
+	transactions []Transaction
+	timestamp    int64
+}
 
 // BC = Linked list of block structs
+var blockchain []Block
 
 // Mine function
 // Create new block
 // Verify chain
-// Add transaction
-package main
 
-func main() int {
-	fmt.PrintLn("LOL")
-	return 0
+/// --- Block Function ---
+
+// Block initialization
+func initBlock(nonce uint64, timestamp int64) Block {
+	b := Block{}
+	b.nonce = nonce
+	b.timestamp = timestamp
+	return b
+}
+
+// Add Transaction
+func (block Block) addTransaction(from [32]byte, to [32]byte, data string, amount float32) Block {
+	block.transactions = append(block.transactions, Transaction{from, to, data, amount})
+	return block
+}
+
+// HERE BEGINS THE MAGIC
+func main() {
+
+	addr_1 := sha256.Sum256([]byte("ADRESS 1"))
+	addr_2 := sha256.Sum256([]byte("ADRESS 2"))
+	now := time.Now()
+
+	// Create genesis block
+	genesis_block := initBlock(0, now.UnixNano()).addTransaction(addr_1, addr_2, "This is the first transaction", 1)
+
+	// Push genesis block
+	blockchain = append(blockchain, genesis_block)
+	println(genesis_block.nonce, genesis_block.timestamp, genesis_block.transactions, blockchain)
+
 }
