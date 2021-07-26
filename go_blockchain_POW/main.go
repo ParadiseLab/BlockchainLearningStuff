3cafc45ac5e61dbbcc75a35fc803a8683b35f1d5
Brian Sinquin <ppxlab@gmail.com>
2021-07-27 00:54:59 +0200

    functional basic blockchain

--- go_blockchain_POW/main.go
+++ go_blockchain_POW/main.go
@@ -19,10 +19,12 @@ type Block struct {
 	nonce        uint64
 	transactions []Transaction
 	timestamp    int64
+	prev_hash    [32]byte
 }
 
-// BC = Linked list of block structs
-var blockchain []Block
+type Blockchain struct {
+	blocks []Block
+}
 
 // Mine function
 // Create new block
@@ -38,14 +40,72 @@ func initBlock(nonce uint64, timestamp int64) Block {
 	return b
 }
 
+func transactionsHash(trans_list []Transaction) uint64 {
+	return 0
+}
+
+// Block Hash
+func (b Block) getBlockHash() [32]byte {
+	block_str := string(b.nonce) + string(b.prev_hash[:]) + string(b.timestamp) + string(transactionsHash(b.transactions))
+	return sha256.Sum256([]byte(block_str))
+}
+
+func (bc Blockchain) verifyBlock(index uint) bool {
+	if index == 0 {
+		panic("Cannot verify genesis block!")
+	}
+	block_1 := bc.blocks[index-1]
+	block_2 := bc.blocks[index]
+
+	if block_2.prev_hash != block_1.getBlockHash() {
+		return false
+	}
+	if block_2.getBlockHash()[0] != '0' {
+		return false
+	}
+	return true
+}
+
+// set block hash
+func (block *Block) setHash(hash [32]byte) {
+	block.prev_hash = hash
+}
+
+// set block nonce
+func (block *Block) setNonce(nonce uint64) {
+	block.nonce = nonce
+}
+
 // Add Transaction
 func (block Block) addTransaction(from [32]byte, to [32]byte, data string, amount float32) Block {
 	block.transactions = append(block.transactions, Transaction{from, to, data, amount})
 	return block
 }
 
+func (bc *Blockchain) addBlock(block Block) {
+	bc.blocks = append(bc.blocks, block)
+}
+
+func (bs *Blockchain) mineLast() {
+	l := len(bs.blocks)
+	if l < 2 {
+		return
+	}
+
+	var i uint64 = 0
+
+	for !bs.verifyBlock(uint(l - 1)) {
+		i++
+		bs.blocks[l-1].setNonce(i)
+	}
+
+	println(i)
+}
+
 // HERE BEGINS THE MAGIC
 func main() {
+
+	blockchain := Blockchain{}
 
 	addr_1 := sha256.Sum256([]byte("ADRESS 1"))
 	addr_2 := sha256.Sum256([]byte("ADRESS 2"))
@@ -53,9 +113,30 @@ func main() {
 
 	// Create genesis block
 	genesis_block := initBlock(0, now.UnixNano()).addTransaction(addr_1, addr_2, "This is the first transaction", 1)
-
+	block_2 := initBlock(0, now.UnixNano()).addTransaction(addr_2, addr_1, "This is the second transaction", 1)
+	block_2.setHash(genesis_block.getBlockHash())
 	// Push genesis block
-	blockchain = append(blockchain, genesis_block)
-	println(genesis_block.nonce, genesis_block.timestamp, genesis_block.transactions, blockchain)
+	blockchain.addBlock(genesis_block)
+	// Push second block
+	blockchain.addBlock(block_2)
+
+	/*  Verification and proof of work
+
+	- each block should contain the hash of the previous block in the "prev_hash" field
+	- the nonce of each block has to be chosen to make the block's hash begin by the null character (proof of work by spoofing)
+	- TODO : adjustable difficulty = number of hash bytes that should be 0 or another constant pattern
+	- TODO : refactor code
+	- TODO :  add MerkleTrees and better data visualisation/manipulation
+
+	*/
+
+	// is the second block correct ?
+	println(blockchain.verifyBlock(1))
+
+	// then mine the block
+	blockchain.mineLast()
+
+	// it should be correct now
+	println(blockchain.verifyBlock(1))
 
 }
