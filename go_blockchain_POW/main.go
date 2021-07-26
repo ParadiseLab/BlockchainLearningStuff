c4f615ee23a1961be7548f9b12da0d694e8bb2cf
Brian Sinquin <ppxlab@gmail.com>
2021-07-26 21:35:24 +0200

    initial commit

---
+++ go_blockchain_POW/main.go
@@ -0,0 +1,15 @@
+// Create transaction struct
+// Create block struct
+
+// BC = Linked list of block structs
+
+// Mine function
+// Create new block
+// Verify chain
+// Add transaction
+package main
+
+func main() int {
+	fmt.PrintLn("LOL")
+	return 0
+}
